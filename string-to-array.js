13c8a9316b2747dcfc8249590cb1e51405750155
Timothy Robards <timothyrobards@gmail.com>
2018-07-24 17:33:29 -0400

    Add files via upload

---
+++ string-to-array.js
@@ -0,0 +1,13 @@
+function stringToArray(string){
+	return string = string.split(",");
+}
+
+/* alternatives 
+function stringToArray(string){
+  return [...string]
+  //return string.match(/\w+/g);
+  //return string.split(/ /);
+  //return string.split(' ');
+  //return string.split(/\s/); 
+  }
+*/
