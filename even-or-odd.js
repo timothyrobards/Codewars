8668bc60a20548c5898ee903d694dd8c5f2c11af
Timothy Robards <timothyrobards@gmail.com>
2018-08-09 21:22:22 -0400

    Add files via upload

---
+++ even-or-odd.js
@@ -0,0 +1,10 @@
+function even_or_odd(number) {
+
+// if number divided by 2 has no remainder its even, otherwise its odd
+
+  if (number % 2 == 0)  
+        return "Even";
+    else {
+        return "Odd";
+  }
+}
